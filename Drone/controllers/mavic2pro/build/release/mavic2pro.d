be2b5086a3b8f7b0c0409217610ab9ccee8bd4e9
Mukil Ram <mukil@Mukils-MacBook-Pro.local>
2024-12-16 01:14:24 +0530

    first commit

---
+++ Drone/controllers/mavic2pro/build/release/mavic2pro.d
@@ -0,0 +1,14 @@
+build/release/mavic2pro.o: mavic2pro.c \
+ C:\Users\utkar\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\utkar\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\utkar\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\utkar\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\utkar\AppData\Local\Programs\Webots/include/controller/c/webots/camera.h \
+ C:\Users\utkar\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Users\utkar\AppData\Local\Programs\Webots/include/controller/c/webots/compass.h \
+ C:\Users\utkar\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h \
+ C:\Users\utkar\AppData\Local\Programs\Webots/include/controller/c/webots/gyro.h \
+ C:\Users\utkar\AppData\Local\Programs\Webots/include/controller/c/webots/inertial_unit.h \
+ C:\Users\utkar\AppData\Local\Programs\Webots/include/controller/c/webots/keyboard.h \
+ C:\Users\utkar\AppData\Local\Programs\Webots/include/controller/c/webots/led.h \
+ C:\Users\utkar\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h
